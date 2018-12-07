64e6ccfeac072e449b883437f7cdc5f00eaf5d85
Peter <peterbupt@gmail.com>
2018-12-07 22:02:32 +0800

    Update and rename test to test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+cd /root/code/ga/ga_cvrp_test_Chong_noSaing/
+nohup python cvrp_runner.py > ../ga_cvrp_test_Chong_noSaing.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_Chong_noSaing2/
+nohup python cvrp_runner.py > ../ga_cvrp_test_Chong_noSaing2.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_Chong_Saing/
+nohup python cvrp_runner.py > ../ga_cvrp_test_Chong_Saing.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_Chong_Saing2/
+nohup python cvrp_runner.py > ../ga_cvrp_test_Chong_Saing2.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_noChong_noSaing/
+nohup python cvrp_runner.py > ../ga_cvrp_test_noChong_noSaing.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_noChong_noSaing2/
+nohup python cvrp_runner.py > ../ga_cvrp_test_noChong_noSaing2.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_noChong_Saing/
+nohup python cvrp_runner.py > ../ga_cvrp_test_noChong_Saing.log 2>&1 &
+
+cd /root/code/ga/ga_cvrp_test_noChong_Saing2/
+nohup python cvrp_runner.py > ../ga_cvrp_test_noChong_Saing2.log 2>&1 &
